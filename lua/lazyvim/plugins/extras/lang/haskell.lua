56578e372266a1699a492d264f2d4e203198769b
Zephyr Lykos <git@mochaa.ws>
2023-08-10 20:47:20 +0800

    extras/lang: ensure haskell debug adapter is installed

--- lua/lazyvim/plugins/extras/lang/haskell.lua
+++ lua/lazyvim/plugins/extras/lang/haskell.lua
@@ -27,4 +27,21 @@ return {
 			desc = "Eval All",
 		},
 	},
+	{
+		"mfussenegger/nvim-dap",
+		optional = true,
+		dependencies = {
+			{
+				"mason.nvim",
+				opts = function(_, opts)
+					opts.ensure_installed = opts.ensure_installed or {}
+					vim.list_extend(opts.ensure_installed, { "haskell-debug-adapter" })
+				end,
+			},
+			{
+				"leoluz/nvim-dap-go",
+				config = true,
+			},
+		},
+	},
 }
