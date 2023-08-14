b39d5c8375c87134597bfcc690bd783dd8d4f088
Zephyr Lykos <git@mochaa.ws>
2023-08-14 18:41:03 +0800

    lsp: enable icons & inlay hints

--- lua/lazyvim/plugins/lsp/init.lua
+++ lua/lazyvim/plugins/lsp/init.lua
@@ -24,10 +24,7 @@ return {
 				virtual_text = {
 					spacing = 4,
 					source = "if_many",
-					prefix = "●",
-					-- this will set set the prefix to a function that returns the diagnostics icon based on the severity
-					-- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
-					-- prefix = "icons",
+					prefix = "icons",
 				},
 				severity_sort = true,
 			},
@@ -35,7 +32,7 @@ return {
 			-- Be aware that you also will need to properly configure your LSP server to
 			-- provide the inlay hints.
 			inlay_hints = {
-				enabled = false,
+				enabled = true,
 			},
 			-- add any global capabilities here
 			capabilities = {},
