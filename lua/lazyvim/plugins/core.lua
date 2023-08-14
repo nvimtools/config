376f01639b0474cf9cbf27b12cc5c93fa6149231
Zephyr Lykos <git@mochaa.ws>
2023-08-14 19:00:06 +0800

    daze: migrate to monorepo

--- lua/lazyvim/plugins/core.lua
+++ lua/lazyvim/plugins/core.lua
@@ -2,5 +2,5 @@ require("lazyvim.config").init()
 
 return {
 	{ "folke/lazy.nvim", version = "*" },
-	{ "nvimtools/daze", priority = 10000, lazy = false, config = true, cond = true, version = "*" },
+	{ "daze", priority = 10000, lazy = false, config = true, cond = true },
 }
--- lua/lazyvim/plugins/extras/vscode.lua
+++ lua/lazyvim/plugins/extras/vscode.lua
@@ -37,7 +37,7 @@ vim.api.nvim_create_autocmd("User", {
 
 return {
 	{
-		"nvimtools/daze",
+		"daze",
 		config = function(_, opts)
 			opts = opts or {}
 			-- disable the colorscheme
