370965c901e75dbb0d828ae37cbeb4007f1dc6ef
Zephyr Lykos <git@mochaa.ws>
2023-09-05 14:39:05 +0800

    ui/indent: use indent-blankline v3

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -200,42 +200,23 @@ return {
 	-- indent guides for Neovim
 	{
 		"lukas-reineke/indent-blankline.nvim",
+		branch = "v3",
 		event = { "BufReadPost", "BufNewFile" },
 		opts = {
-			-- char = "▏",
-			char = "│",
-			filetype_exclude = {
-				"help",
-				"alpha",
-				"dashboard",
-				"neo-tree",
-				"Trouble",
-				"lazy",
-				"mason",
-				"notify",
-				"toggleterm",
-				"lazyterm",
-			},
-			show_trailing_blankline_indent = false,
-			show_current_context = false,
-		},
-	},
-
-	-- Active indent guide and indent text objects. When you're browsing
-	-- code, this highlights the current level of indentation, and animates
-	-- the highlighting.
-	{
-		"echasnovski/mini.indentscope",
-		version = false, -- wait till new 0.7.0 release to put it back on semver
-		event = { "BufReadPre", "BufNewFile" },
-		opts = {
-			-- symbol = "▏",
-			symbol = "│",
-			options = { try_as_border = true },
-		},
-		init = function()
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = {
+			exclude = {
+				buftypes = {
+					"terminal",
+					"nofile",
+					"quickfix",
+					"prompt",
+				},
+				filetypes = {
+					"lspinfo",
+					"checkhealth",
+					"man",
+					"gitcommit",
+					"TelescopePrompt",
+					"TelescopeResults",
 					"help",
 					"alpha",
 					"dashboard",
@@ -246,12 +227,11 @@ return {
 					"notify",
 					"toggleterm",
 					"lazyterm",
+					"",
 				},
-				callback = function()
-					vim.b.miniindentscope_disable = true
-				end,
-			})
-		end,
+			},
+			show_current_context = false,
+		},
 	},
 
 	-- noicer ui
