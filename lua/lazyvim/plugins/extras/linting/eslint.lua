66cf857710a0fea164b489bb3d89ef7cfaef8dea
Zephyr Lykos <git@mochaa.ws>
2023-09-30 01:27:34 +0800

    lint/eslint: fix passing lsp config

--- lua/lazyvim/plugins/extras/linting/eslint.lua
+++ lua/lazyvim/plugins/extras/linting/eslint.lua
@@ -7,8 +7,10 @@ return {
 			servers = {
 				eslint = {
 					settings = {
-						-- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
-						workingDirectory = { mode = "auto" },
+						eslint = {
+							-- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+							workingDirectory = { mode = "auto" },
+						},
 					},
 				},
 			},
