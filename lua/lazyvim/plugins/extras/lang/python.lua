8b34706d481257e849023f012734a403e313a188
Zephyr Lykos <git@mochaa.ws>
2023-10-28 10:08:07 +0800

    lang/python: remove pyright

--- lua/lazyvim/plugins/extras/lang/python.lua
+++ lua/lazyvim/plugins/extras/lang/python.lua
@@ -11,18 +11,7 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        pyright = {},
         ruff_lsp = {},
-      },
-      setup = {
-        ruff_lsp = function()
-          require("lazyvim.util").on_attach(function(client, _)
-            if client.name == "ruff_lsp" then
-              -- Disable hover in favor of Pyright
-              client.server_capabilities.hoverProvider = false
-            end
-          end)
-        end,
       },
     },
   },
